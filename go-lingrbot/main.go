b4465ccbfac28ad87cf022135d26807a4923fee1
mattn <mattn.jp@gmail.com>
2013-04-03 22:42:54 +0900

    Cleanup code

--- go-lingrbot/main.go
+++ go-lingrbot/main.go
@@ -12,6 +12,10 @@ import (
 	"strings"
 )
 
+type PlusPlus struct {
+	Count int
+}
+
 type Status struct {
 	Events []Event `json:"events"`
 }
@@ -32,6 +36,54 @@ type Message struct {
 	Text            string `json:"text"`
 }
 
+var re = regexp.MustCompile(`(?:^|[^a-zA-Z0-9])(https?://[a-zA-Z][a-zA-Z0-9_-]*(\.[a-zA-Z0-9][a-zA-Z0-9_-]*)*(:\d+)?(?:/[a-zA-Z0-9_/.\-+%#?&=;@$,!*~]*)?)`)
+
+func getTitle(client *http.Client, url string) string {
+	r, _ := client.Get(url)
+	if !strings.HasPrefix(r.Header.Get("Content-Type"), "text/html") {
+		return ""
+	}
+	doc, _ := html.Parse(r.Body)
+
+	title := ""
+	charset := ""
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if charset == "" && n.Type == html.ElementNode && n.Data == "meta" {
+			kv := make(map[string]string)
+			for _, a := range n.Attr {
+				kv[strings.ToLower(a.Key)] = strings.ToLower(a.Val)
+			}
+			if v, ok := kv["http-equiv"]; ok && v == "content-type" {
+				if v, ok = kv["content"]; ok {
+					for _, t := range strings.Split(v, ";") {
+						tt := strings.Split(strings.TrimSpace(t), "=")
+						if len(tt) == 2 && strings.ToLower(tt[0]) == "charset" {
+							charset = tt[1]
+							break
+						}
+					}
+				}
+			}
+			if v, ok := kv["charset"]; ok {
+				charset = v
+			}
+		}
+		if n.Type == html.ElementNode && n.Data == "title" {
+			if charset == "" {
+				charset = "utf-8"
+			}
+			title = mahonia.NewDecoder(charset).ConvertString(n.FirstChild.Data)
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
+
+	return title
+}
+
 func init() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/" {
@@ -50,54 +102,14 @@ func init() {
 					tokens := strings.SplitN(event.Message.Text, " ", 2)
 					if tokens[0] == "!go" {
 						w.Write([]byte("日本goでok"))
-					} else if strings.HasPrefix(r.Header.Get("Content-Type"), "text/html") {
-						re := regexp.MustCompile(`(?:^|[^a-zA-Z0-9])(https?://[a-zA-Z][a-zA-Z0-9_-]*(\.[a-zA-Z0-9][a-zA-Z0-9_-]*)*(:\d+)?(?:/[a-zA-Z0-9_/.\-+%#?&=;@$,!*~]*)?)`)
+					} else {
 						ss := re.FindAllStringSubmatch(event.Message.Text, -1)
 
 						results := ""
 						for _, s := range ss {
-							url := s[1]
-							r, _ := u.Get(url)
-							doc, _ := html.Parse(r.Body)
-
-							charset := ""
-							var f func(*html.Node)
-							f = func(n *html.Node) {
-								if charset == "" && n.Type == html.ElementNode && n.Data == "meta" {
-									kv := make(map[string]string)
-									for _, a := range n.Attr {
-										kv[strings.ToLower(a.Key)] = strings.ToLower(a.Val)
-									}
-									if v, ok := kv["http-equiv"]; ok {
-										if v == "content-type" {
-											if v, ok = kv["content"]; ok {
-												for _, t := range strings.Split(v, ";") {
-													tt := strings.Split(strings.TrimSpace(t), "=")
-													if len(tt) == 2 && strings.ToLower(tt[0]) == "charset" {
-														charset = tt[1]
-														break
-													}
-												}
-											}
-										}
-									}
-									if v, ok := kv["charset"]; ok {
-											charset = v
-									}
-								}
-								if n.Type == html.ElementNode && n.Data == "title" {
-									if charset == "" {
-										charset = "utf-8"
-									}
-									title := mahonia.NewDecoder(charset).ConvertString(n.FirstChild.Data)
-									results += "Title: " + title + "\n"
-									return
-								}
-								for c := n.FirstChild; c != nil; c = c.NextSibling {
-									f(c)
-								}
+							if title := getTitle(u, s[1]); title != "" {
+								results += "Title: " + title + "\n"
 							}
-							f(doc)
 						}
 						if len(results) > 0 {
 							w.Write([]byte(results))
