a6d989474bcb50ff3a4517947a917839c2796c6f
mattn <mattn.jp@gmail.com>
2013-04-15 12:24:31 +0900

    sudden death

--- go-lingrbot/main.go
+++ go-lingrbot/main.go
@@ -56,6 +56,7 @@ var rePlus = regexp.MustCompile(`^\s*([a-zA-Z0-9_{^}]+)\+\+\s*$`)
 var reMinus = regexp.MustCompile(`^\s*([a-zA-Z0-9_{^}]+)--\s*$`)
 var rePlusEq = regexp.MustCompile(`^\s*([a-zA-Z0-9_{^}]+)\+=([0-9])\s*$`)
 var reMinusEq = regexp.MustCompile(`^\s*([a-zA-Z0-9_{^}]+)\-=([0-9])\s*$`)
+var reSuddenDeath = regexp.MustCompile(`^突然の.+$`)
 
 func atoi(a string) int {
 	i, _ := strconv.Atoi(a)
@@ -177,6 +178,51 @@ func parsePlusPlus(message string, callback func(nick string, plus int)) bool {
 	return false
 }
 
+func runeWidth(r rune) int {
+	if r >= 0x1100 &&
+		(r <= 0x115f || r == 0x2329 || r == 0x232a ||
+			(r >= 0x2e80 && r <= 0xa4cf && r != 0x303f) ||
+			(r >= 0xac00 && r <= 0xd7a3) ||
+			(r >= 0xf900 && r <= 0xfaff) ||
+			(r >= 0xfe30 && r <= 0xfe6f) ||
+			(r >= 0xff00 && r <= 0xff60) ||
+			(r >= 0xffe0 && r <= 0xffe6) ||
+			(r >= 0x20000 && r <= 0x2fffd) ||
+			(r >= 0x30000 && r <= 0x3fffd)) {
+		return 2
+	}
+	return 1
+}
+
+func strWidth(str string) int {
+	r := 0
+	for _, c := range ([]rune(str)) {
+		r += runeWidth(c)
+	}
+	return r
+}
+
+func suddenDeath(msg string) string {
+	lines := strings.Split(msg, "\n")
+	widths := []int{}
+
+	maxWidth := 0
+	for _, line := range lines {
+		width := strWidth(line)
+		widths = append(widths, width)
+		if maxWidth < width {
+			maxWidth = width
+		}
+	}
+
+	ret := "＿" + strings.Repeat("人", maxWidth / 2) + "＿\n"
+	for i, line := range lines {
+		ret += "＞" + line + strings.Repeat(" ", maxWidth - widths[i]) + "＜\n"
+	}
+	ret += "￣" + strings.Repeat("Ｙ", maxWidth / 2) + "￣\n"
+	return ret
+}
+
 func init() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/" {
@@ -204,6 +250,8 @@ func init() {
 						} else {
 							results = "No such documents\n"
 						}
+					} else if reSuddenDeath.MatchString(event.Message.Text) {
+						results += suddenDeath(event.Message.Text)
 					} else {
 						ss := reUrl.FindAllStringSubmatch(event.Message.Text, -1)
 
